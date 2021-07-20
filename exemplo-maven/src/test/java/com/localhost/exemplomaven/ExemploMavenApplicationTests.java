5c3655715ceba08cc635680c67ff2bbcae730618
Maic Miller <maicmiller42@gmail.com>
2021-07-19 22:44:40 -0300

    First commit
    Create project base

---
+++ exemplo-maven/src/test/java/com/localhost/exemplomaven/ExemploMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localhost.exemplomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
